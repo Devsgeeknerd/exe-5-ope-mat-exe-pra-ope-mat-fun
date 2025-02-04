10eb9e3058551a901cf05adb48e1ae10ad84627d
Paulo Vanderley <devsgeeknerd@icloud.com>
2025-02-04 12:32:40 -0300

    :sparkles: feat(main): implementar leitura do salário atual no main.c.

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
 // Inclui a biblioteca para as funções de entrada e saída (como printf e scanf).
 #include <stdio.h>
 
-void main() {}
+void main() {
+    // Declara a variável 'salario' para armazenar o valor do salário.
+    float salario;
+
+    // Declara a variável 'reajuste' para armazenar o percentual do reajuste.
+    float reajuste;
+
+    // Declara a variável 'novoSalario' para armazenar o novo valor do salário após o reajuste.
+    float novoSalario;
+
+    // Solicita ao usuário o valor do salário atual.
+    printf("Digite o valor do salário atual: ");
+
+    // Lê o valor informado pelo usuário e armazena em 'salario'.
+    scanf("%f", &salario);
+}
